de7d4e798a3fc451e9ed0b0946f6b51e15ad5d8f
simonbing <bings@student.ethz.ch>
2021-05-04 17:44:24 +0200

    Training running locally

--- scripts/run_gru_d.py
+++ scripts/run_gru_d.py
@@ -7,15 +7,17 @@ import math
 import warnings
 import itertools
 import numbers
-import torch.utils.data as utils
+# import torch.utils.data as utils
+from torch.utils.data import TensorDataset, DataLoader
 #import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc, roc_auc_score, average_precision_score
 
 import sys
 sys.path.insert(0, '../src')
-from data_loader_oo import DataContainer
+# from data_loader_oo import DataContainer
 from GRUD_model import grud_model
 
+### NEW IMPORTS FOR MEDGEN ###
 
 
 ex = Experiment("GRU-D-mean")
@@ -24,11 +26,11 @@ device = torch.device("cuda:0" if use_cuda else "cpu")
 
 #TODO: data gets padded before, does this have any effect if i just take the last step output as output?
 
-def fit(model, criterion, l2_penalty, learning_rate,\
-        train_dataloader, dev_dataloader, test_dataloader,\
+def fit(model, criterion, l2_penalty, learning_rate, \
+        train_dataloader, val_dataloader, test_dataloader, \
         learning_rate_decay=0, n_epochs=30, checkpoint_path = 'model_checkpoints', sacred_run=None):
     
-    test_freq = int(len(train_dataloader) / 4)
+    test_freq = int(len(train_dataloader) / 4) # TODO: more sensible validation criterion/freq
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
     
@@ -57,24 +59,26 @@ def fit(model, criterion, l2_penalty, learning_rate,\
         
         
         #get a minibatch
-        for train_data, train_label, train_num_obs in train_dataloader:
+        # for train_data, train_label, train_num_obs in train_dataloader:
+        for train_data, train_label in train_dataloader:
             model.train()
             #push current sample to GPU or CPU
-            train_data, train_label, train_num_obs = train_data.to(device), train_label.to(device), train_num_obs.to(device)
+            # train_data, train_label, train_num_obs = train_data.to(device), train_label.to(device), train_num_obs.to(device)
+            train_data, train_label= train_data.to(device), train_label.to(device)
 
 
             # Zero the parameter gradients
             optimizer.zero_grad()
             
             # Forward pass : Compute predicted y by passing train data to the model
-            #y_pred = model(train_data)[:,-1,:] #last output
+            y_pred = model(train_data)[:,-1,:] #last output
 
             #with pad after:
-            y_pred = model(train_data)
+            # y_pred = model(train_data)
             #print(y_pred.size())
             y_pred = torch.squeeze(y_pred)
             #print(y_pred.size())
-            y_pred = torch.gather(y_pred,1,train_num_obs.long())
+            # y_pred = torch.gather(y_pred,1,train_num_obs.long())
             #print(y_pred.size())
             #print(train_num_obs.size())
 
@@ -109,41 +113,43 @@ def fit(model, criterion, l2_penalty, learning_rate,\
                 label, pred = np.array([]), np.array([])
                 model.eval()
 
-                for dev_data, dev_label, dev_num_obs in dev_dataloader:
+                # for val_data, val_label, dev_num_obs in val_dataloader:
+                for val_data, val_label in val_dataloader:
 
-                    dev_data, dev_label, dev_num_obs = dev_data.to(device), dev_label.to(device), dev_num_obs.to(device)
+                    # val_data, val_label, dev_num_obs = val_data.to(device), val_label.to(device), dev_num_obs.to(device)
+                    val_data, val_label = val_data.to(device), val_label.to(device)
 
-                    print(dev_data.size())
-                    print(dev_label.size())
-                    print(dev_num_obs.size())
+                    print(val_data.size())
+                    print(val_label.size())
+                    # print(dev_num_obs.size())
 
                     optimizer.zero_grad()
                     # Forward pass : Compute predicted y by passing train data to the model
     
-                    #y_pred = model(dev_data)[:,-1,:] #last output
-                    y_pred = model(dev_data)
+                    y_pred = model(val_data)[:,-1,:] #last output
+                    # y_pred = model(val_data)
                     y_pred = torch.squeeze(y_pred)
                     #print(y_pred.size())
-                    y_pred = torch.gather(y_pred,1,dev_num_obs.long())
+                    # y_pred = torch.gather(y_pred,1,dev_num_obs.long())
 
                     # Compute loss
-                    loss = criterion(y_pred, dev_label)
+                    loss = criterion(y_pred, val_label)
                     acc.append(
                         torch.eq(
                             (torch.sigmoid(y_pred).data > 0.5).float(),
-                            dev_label)
+                            val_label)
                     )
                     losses.append(loss.item())
 
-                    label = np.append(label, dev_label.detach().cpu().numpy())
+                    label = np.append(label, val_label.detach().cpu().numpy())
                     pred = np.append(pred, y_pred.detach().cpu().numpy())
 
 
-                dev_acc = torch.mean(torch.cat(acc).float())
-                dev_loss = np.mean(losses)
+                val_acc = torch.mean(torch.cat(acc).float())
+                val_loss = np.mean(losses)
                 
-                dev_pred_out = pred
-                dev_label_out = label
+                val_pred_out = pred
+                val_label_out = label
 
                 va_auc = roc_auc_score(label, pred)
                 va_prc = average_precision_score(label, pred)   
@@ -161,9 +167,9 @@ def fit(model, criterion, l2_penalty, learning_rate,\
                 AU_PRCs.append(va_prc)        
                 train_loss = np.mean(losses)
                 # print("Epoch: {} Train: {:.4f}/{:.2f}%, Dev: {:.4f}/{:.2f}%, Test: {:.4f}/{:.2f}% AUC: {:.4f}".format(
-                #     epoch, train_loss, train_acc*100, dev_loss, dev_acc*100, test_loss, test_acc*100, auc_score))
+                #     epoch, train_loss, train_acc*100, val_loss, val_acc*100, test_loss, test_acc*100, auc_score))
                 print("Epoch: {} Train loss: {:.4f}, Dev loss: {:.4f}, Val PRC: {:.4f}".format(
-                    epoch, train_loss, dev_loss, va_prc))
+                    epoch, train_loss, val_loss, va_prc))
 
         
         train_acc = torch.mean(torch.cat(acc).float())
@@ -188,44 +194,44 @@ def fit(model, criterion, l2_penalty, learning_rate,\
         losses, acc = [], []
         label, pred = np.array([]), np.array([])
         model.eval()
-        for dev_data, dev_label, num_obs in dev_dataloader:
+        for val_data, val_label, num_obs in dev_dataloader:
             # Squeeze the data [1, 33, 49], [1,5] to [33, 49], [5]
-            #dev_data = torch.squeeze(dev_data)
-            #dev_label = torch.squeeze(dev_label)
-            dev_data, dev_label, = dev_data.to(device), dev_label.to(device)
+            #val_data = torch.squeeze(val_data)
+            #val_label = torch.squeeze(val_label)
+            val_data, val_label, = val_data.to(device), val_label.to(device)
 
             optimizer.zero_grad()
             # Forward pass : Compute predicted y by passing train data to the model
-            #print(dev_data.size())
+            #print(val_data.size())
             #print(model.w_dg_x.weight)
             #print(model.w_dg_x.bias)
-            y_pred = model(dev_data)[:,-1,:] #last output
+            y_pred = model(val_data)[:,-1,:] #last output
             #print(y_pred.size())
             
             # Save predict and label
             #pred.append(y_pred.item())
-            #label.append(dev_label.item())
+            #label.append(val_label.item())
 
             # Compute loss
-            loss = criterion(y_pred, dev_label)
+            loss = criterion(y_pred, val_label)
             acc.append(
                 torch.eq(
                     (torch.sigmoid(y_pred).data > 0.5).float(),
-                    dev_label)
+                    val_label)
             )
             losses.append(loss.item())
 
             # Save predict and label
-            #print(dev_label.detach().numpy().shape)
+            #print(val_label.detach().numpy().shape)
             #print(y_pred.detach().numpy().shape)
-            label = np.append(label, dev_label.detach().cpu().numpy())
+            label = np.append(label, val_label.detach().cpu().numpy())
             pred = np.append(pred, y_pred.detach().cpu().numpy())
             #print(label.shape)
 
-        dev_acc = torch.mean(torch.cat(acc).float())
-        #print(dev_acc)
+        val_acc = torch.mean(torch.cat(acc).float())
+        #print(val_acc)
         #print(label.shape)
-        dev_loss = np.mean(losses)
+        val_loss = np.mean(losses)
         
         dev_pred_out = pred
         dev_label_out = label
@@ -240,9 +246,9 @@ def fit(model, criterion, l2_penalty, learning_rate,\
 
         AU_PRCs[epoch] = va_prc        
         # print("Epoch: {} Train: {:.4f}/{:.2f}%, Dev: {:.4f}/{:.2f}%, Test: {:.4f}/{:.2f}% AUC: {:.4f}".format(
-        #     epoch, train_loss, train_acc*100, dev_loss, dev_acc*100, test_loss, test_acc*100, auc_score))
+        #     epoch, train_loss, train_acc*100, val_loss, val_acc*100, test_loss, test_acc*100, auc_score))
         print("Epoch: {} Train loss: {:.4f}, Dev loss: {:.4f}, Val PRC: {:.4f}".format(
-            epoch, train_loss, dev_loss, va_prc))
+            epoch, train_loss, val_loss, va_prc))
         """
         
         # save the parameters
@@ -259,8 +265,11 @@ def fit(model, criterion, l2_penalty, learning_rate,\
 @ex.config
 def gru_d_config():
     #data config
-    file_path = "../mgp-rnn-datadump_labs_vitals_covs_na_thres_500_min_length_7_max_length_200_horizon_0_split_0.pkl"
+    file_path = "/Users/Simon/Documents/Uni/MasterThesis/datasets/mimiciii/15_mins_test"
     data_sources = ['labs','vitals','covs']
+
+    features_path = '/Users/Simon/Documents/Uni/MasterThesis/datasets/mimiciii/15_mins_test/X_6hrs_15min_100_vent_bin.npz'
+    labels_path = '/Users/Simon/Documents/Uni/MasterThesis/datasets/mimiciii/15_mins_test/y_6hrs_15min_100_vent_bin.npz'
     
 
     #model config
@@ -269,38 +278,59 @@ def gru_d_config():
 
     #training config
     n_epochs = 40
-    batch_size = 100
+    batch_size = 10
     learning_rate = 0.001   
-    learning_rate_decay =7
+    learning_rate_decay = 7
     dropout = 0.1
     l2_penalty = 0.001
 
 def count_parameters(model: torch.nn.Module):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def reshape_for_grud_d(X):
+    """
+    Args:
+        X: dictionary containing features, missingness mask and delta_t arrays
+    Retruns:
+        train_data, val_data, test_data: [N_patients, [input, m_mask, delta_t], features, time_len]
+    """
+    train_data = np.stack((X['X_train'], X['m_train'], X['delta_t_train']), axis=1)
+    val_data = np.stack((X['X_val'], X['m_val'], X['delta_t_val']), axis=1)
+    test_data = np.stack((X['X_test'], X['m_test'], X['delta_t_test']), axis=1)
+
+    return train_data, val_data, test_data
+
 @ex.main
-def run_gru_d(_run, file_path, data_sources, batch_size, n_epochs, hidden_size, n_layers, learning_rate, learning_rate_decay, l2_penalty, dropout):
+def run_gru_d(_run, file_path, features_path, labels_path, data_sources, batch_size, n_epochs, hidden_size, n_layers, learning_rate, learning_rate_decay, l2_penalty, dropout):
+    X = np.load(features_path)
+    y = np.load(labels_path)
 
 
     #setup data
-    data = DataContainer(file_path,data_sources) #problem: need same padding for train and va data
-    va_data = data.va_data_gru_d()
-    tr_data = data.tr_data_gru_d()
-    x_mean = tr_data[:,0,:,:].mean(axis=0).mean(axis=1)
+    # data = DataContainer(file_path,data_sources) #problem: need same padding for train and va data
+    # va_data = data.va_data_gru_d()
+    # tr_data = data.tr_data_gru_d()
+    train_data, val_data, test_data = reshape_for_grud_d(X)
 
-    train_dataset = torch.utils.data.TensorDataset(torch.Tensor(tr_data),torch.Tensor(np.expand_dims(data.labels_tr, axis =1)),torch.Tensor(np.expand_dims(data.num_obs_times_tr-1, axis =1)))
-    val_dataset = torch.utils.data.TensorDataset(torch.Tensor(va_data), torch.Tensor(np.expand_dims(data.labels_va, axis =1)),torch.Tensor(np.expand_dims(data.num_obs_times_va-1, axis =1)))
+    # x_mean = tr_data[:,0,:,:].mean(axis=0).mean(axis=1)
+    x_mean = train_data[:,0,:,:].mean(axis=(0,2))
+
+    # train_dataset = torch.utils.data.TensorDataset(torch.Tensor(tr_data),torch.Tensor(np.expand_dims(data.labels_tr, axis =1)),torch.Tensor(np.expand_dims(data.num_obs_times_tr-1, axis =1)))
+    # val_dataset = torch.utils.data.TensorDataset(torch.Tensor(va_data), torch.Tensor(np.expand_dims(data.labels_va, axis =1)),torch.Tensor(np.expand_dims(data.num_obs_times_va-1, axis =1)))
+
+    train_dataset = TensorDataset(torch.Tensor(train_data), torch.Tensor(y['y_train']))
+    val_dataset = TensorDataset(torch.Tensor(val_data), torch.Tensor(y['y_val']))
+    test_dataset = TensorDataset(torch.Tensor(test_data), torch.Tensor(y['y_test']))
     x_mean = torch.Tensor(x_mean)
 
     #needs int() bc hyperparameter script passes np.int64 and pytorch doesnt like that
     #TODO add num_obs to dataset, then dataloader 
 
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=int(batch_size), shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size= int(batch_size), shuffle=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=int(batch_size), shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size= int(batch_size), shuffle=True)
 
-    
-    print(va_data.shape)
-    print(tr_data.shape)
+    print(train_data.shape)
+    print(val_data.shape)
     print('Data loaded!')
     #print(tr_data[1])
 
@@ -308,11 +338,12 @@ def run_gru_d(_run, file_path, data_sources, batch_size, n_epochs, hidden_size, 
     print("hidden_size: {}, dropout: {}, num_layers: {}".format(hidden_size,dropout,n_layers))
 
     #fit the model
-    input_size = 44 # num of variables
+    # input_size = 44 # num of variables
+    input_size = train_data.shape[2]
     #hidden_size = 100 # same as inputsize
     output_size = 1
     #num_layers = 3 # num of GRU layers (only first layer is GRU-D)
-    seq_len = 1264 # max seq len based on data
+    # seq_len = 1264 # max seq len based on data
     model = grud_model(input_size = input_size, hidden_size= int(hidden_size), output_size=output_size, dropout=dropout, dropout_type='mloss', x_mean=x_mean, num_layers=int(n_layers))
     
   
@@ -323,13 +354,15 @@ def run_gru_d(_run, file_path, data_sources, batch_size, n_epochs, hidden_size, 
         print("Running on GPU")
 
     criterion = torch.nn.BCELoss()
+
+    ### IGNORING WEIGHTED LOSS FOR NOW SINCE WE STRATIFY IN SPLIT ###
     #untested
     #logits=preds, targets=minibatch.O_dupe_onehot, pos_weight=self.class_imb)
     #Get class imbalance (for weighted loss):
-    case_prev = data.labels_tr.sum()/float(len(data.labels_tr)) #get prevalence of cases in train dataset
-    class_imb = torch.tensor(1/case_prev) #class imbalance to use as class weight if losstype='weighted'
-    #TODO then we do not need to apply sigmoid to the model output in the model
-    criterion = torch.nn.BCEWithLogitsLoss(pos_weight=class_imb)
+    # case_prev = data.labels_tr.sum()/float(len(data.labels_tr)) #get prevalence of cases in train dataset
+    # class_imb = torch.tensor(1/case_prev) #class imbalance to use as class weight if losstype='weighted'
+    # #TODO then we do not need to apply sigmoid to the model output in the model
+    # criterion = torch.nn.BCEWithLogitsLoss(pos_weight=class_imb)
      
     
 
